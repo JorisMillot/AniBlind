b1ab6943bbee642bd6439b7582e62eb0df516cb7
kevinfaurie <fauriekevinpro@gmail.com>
2018-10-16 14:21:00 +0200

    Kévin

--- js/accueil.js
+++ js/accueil.js
@@ -2,3 +2,7 @@ function displayGame(){
     document.getElementById("rules").style.display = "none";
     document.getElementById("game").style.display = "block";
 }
+
+function displayNextButton(){
+    document.getElementById("nextButton").style.visibility = "visible";
+}
