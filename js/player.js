209b13f80ff4aec38fec23bf6f10168723ae0e85
JorisMillot <38904722+JorisMillot@users.noreply.github.com>
2018-10-16 15:57:18 +0200

    Perdu

--- js/player.js
+++ js/player.js
@@ -20,21 +20,39 @@ function playAudio(songIndex){
     timestampIndex = Math.floor(timestampIndex);
     player.currentTime = song.timestamp[timestampIndex];
 
-    player.play().then(function (){
+    let isDone = false;
 
+    document.getElementById("validateBtn").onclick = function(){
+        validerReponse();
+        isDone = true;
+    }
+
+    player.play().then(function (){
         document.getElementById("currentTime").innerText = "";
         result = setInterval(function(){document.getElementById("currentTime").innerText = 'Time : '+ (player.currentTime.toFixed(0) - song.timestamp[timestampIndex])+ ' s';},1000);
         document.getElementById("animeImageImg").style.filter = "blur(10px)";
         document.getElementById("animeImageImg").setAttribute("src","src/image/"+song.url_image);
         switch(difficulty){
             case 1: 
-                setTimeout(function() { player.pause(); }, 30000);
+                setTimeout(function() { 
+                    if(isDone !== true){
+                        validerReponse();
+                    }
+                }, 30000);
                 break;
             case 2: 
-                setTimeout(function() { player.pause(); }, 20000);
+                setTimeout(function() { 
+                    if(isDone !== true){
+                        validerReponse();
+                    }
+                }, 20000);
                 break;
             case 3: 
-                setTimeout(function() { player.pause(); }, 10000);
+                setTimeout(function() { 
+                    if(isDone !== true){
+                        validerReponse();
+                    }
+                }, 10000);
                 break;
         }
     });
