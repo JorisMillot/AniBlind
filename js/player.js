088cfe6e0142b49045b499cd6f407b6f263803b7
kevinfaurie <fauriekevinpro@gmail.com>
2018-10-16 17:14:56 +0200

    Kévin

--- js/player.js
+++ js/player.js
@@ -4,65 +4,71 @@ var currentPlayer;
 var result;
 var i;
 
-function nextSong(){
+function nextSong() {
     document.getElementById("myInput").disabled = false;
     document.getElementById("result").style.visibility = "hidden";
-    currentSong = Math.floor(Math.random()*(data.length));
+    currentSong = Math.floor(Math.random() * (data.length));
     playAudio(currentSong);
     document.getElementById("nextButton").style.visibility = "hidden";
 }
 
-function playAudio(songIndex){
+function playAudio(songIndex) {
     let player = new Audio();
     currentPlayer = player;
     var song = data[songIndex];
-    var audiopath = 'src/audio/'+song.url_audio;
+    var audiopath = 'src/audio/' + song.url_audio;
     player.src = audiopath;
-    var timestampIndex = Math.random()*song.timestamp.length;
+    var timestampIndex = Math.random() * song.timestamp.length;
     timestampIndex = Math.floor(timestampIndex);
     player.currentTime = song.timestamp[timestampIndex];
 
     let isDone = false;
 
-    document.getElementById("validateBtn").onclick = function(){
+    document.getElementById("validateBtn").onclick = function () {
         validerReponse();
         isDone = true;
     }
 
-    player.play().then(function (){
+    player.play().then(function () {
         document.getElementById("currentTime").innerText = "";
         document.getElementById("animeImageImg").style.filter = "blur(10px)";
-        document.getElementById("animeImageImg").setAttribute("src","src/image/"+song.url_image);
-        switch(difficulty){
-            case 1: 
+        document.getElementById("animeImageImg").setAttribute("src", "src/image/" + song.url_image);
+        switch (difficulty) {
+            case 1:
                 i = 31;
-                result = setInterval(function(){
+                result = setInterval(function () {
                     i--;
-                    document.getElementById("currentTime").innerText = 'Time : '+ i + ' s';
-                    if(i <= 0){
+                    document.getElementById("currentTime").innerText = 'Time : ' + i + ' s';
+                    if (i <= 0) {
                         validerReponse();
+                    } else if (i === 10) {
+                        document.getElementById("animeImageImg").style.filter = "blur(2px)";
                     }
-                },1000);
+                }, 1000);
                 break;
-            case 2: 
+            case 2:
                 i = 21;
-                result = setInterval(function(){
+                result = setInterval(function () {
                     i--;
-                    document.getElementById("currentTime").innerText = 'Time : '+ i + ' s';
-                    if(i <= 0){
+                    document.getElementById("currentTime").innerText = 'Time : ' + i + ' s';
+                    if (i <= 0) {
                         validerReponse();
+                    } else if (i === 10) {
+                        document.getElementById("animeImageImg").style.filter = "blur(2px)";
                     }
-                },1000);
+                }, 1000);
                 break;
-            case 3: 
+            case 3:
                 i = 11;
-                result = setInterval(function(){
+                result = setInterval(function () {
                     i--;
-                    document.getElementById("currentTime").innerText = 'Time : '+ i + ' s';
-                    if(i <= 0){
+                    document.getElementById("currentTime").innerText = 'Time : ' + i + ' s';
+                    if (i <= 0) {
                         validerReponse();
+                    } else if (i === 3) {
+                        document.getElementById("animeImageImg").style.filter = "blur(2px)";
                     }
-                },1000);
+                }, 1000);
                 break;
         }
     });
