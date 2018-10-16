415dd6000e3285e126b666e894f5e9fea2e61e94
JorisMillot <38904722+JorisMillot@users.noreply.github.com>
2018-10-16 14:24:52 +0200

    fix

--- js/player.js
+++ js/player.js
@@ -14,18 +14,19 @@ function playAudio(songIndex){
     var timestampIndex = Math.random()*song.timestamp.length;
     timestampIndex = Math.floor(timestampIndex);
     player.currentTime = song.timestamp[timestampIndex];
-    player.play();
-    document.getElementById("animeImageImg").style.filter = "blur(10px)";
-    document.getElementById("animeImageImg").setAttribute("src","src/image/"+song.url_image);
-    switch(difficulty){
-        case 1: 
-            setTimeout(function() { player.pause(); }, 30000);
-            break;
-        case 2: 
-            setTimeout(function() { player.pause(); }, 20000);
-            break;
-        case 3: 
-            setTimeout(function() { player.pause(); }, 10000);
-            break;
-    }
+    player.play().then(function (){
+        document.getElementById("animeImageImg").style.filter = "blur(10px)";
+        document.getElementById("animeImageImg").setAttribute("src","src/image/"+song.url_image);
+        switch(difficulty){
+            case 1: 
+                setTimeout(function() { player.pause(); }, 30000);
+                break;
+            case 2: 
+                setTimeout(function() { player.pause(); }, 20000);
+                break;
+            case 3: 
+                setTimeout(function() { player.pause(); }, 10000);
+                break;
+        }
+    });
 }
